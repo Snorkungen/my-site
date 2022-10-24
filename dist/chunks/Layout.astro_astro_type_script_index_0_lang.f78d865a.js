2556873497efcbc24e208578cfd2ab7fbb1ef99c
snorkungen <teuvo2004@gamil.com>
2022-10-24 16:17:02 +0300

    Build

---
+++ dist/chunks/Layout.astro_astro_type_script_index_0_lang.f78d865a.js
@@ -0,0 +1,1 @@
+const e=document.getElementById("last-updated-time");function o(t){let n=Number(t.textContent);t.textContent=new Date(n).toDateString()}e&&o(e);
---
+++ dist/hoisted.bb6eef47.js
@@ -0,0 +1,1 @@
+import"./chunks/Layout.astro_astro_type_script_index_0_lang.f78d865a.js";console.log();
